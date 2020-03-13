0440bf1bd7331298fca5c6f015176ab778b0c9f7
Karthi Keyan <karthikeyan027@gmail.com>
2020-03-13 12:15:04 +0800

    Migrate to androidX

--- app/src/main/java/com/dagger/raju/myapplication/di/AppModule.java
+++ app/src/main/java/com/dagger/raju/myapplication/di/AppModule.java
@@ -1,7 +1,7 @@
 package com.dagger.raju.myapplication.di;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import javax.inject.Singleton;
 
--- app/src/main/java/com/dagger/raju/myapplication/di/UtilsModule.java
+++ app/src/main/java/com/dagger/raju/myapplication/di/UtilsModule.java
@@ -1,7 +1,7 @@
 package com.dagger.raju.myapplication.di;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.dagger.raju.myapplication.network.NetworkChannel;
 import com.dagger.raju.myapplication.utils.NetworkUtil;
